0f364cbbf99e09117c4d044afdf1b5a84efd0a94
harryfyodor <wuhaiweideyouxiang@163.com>
2016-03-25 18:12:13 +0800

    add harryfyodor update warmup

--- warmup/js/sort.js
+++ warmup/js/sort.js
@@ -8,7 +8,7 @@ function sortByTask(bigTask) {
 		content.push(demos[i].innerHTML);
 	}
 
-	for (var j = 0; j < 12; j++) {
+	for (var j = 0; j < 49; j++) {
 		var taskIndex = getTitle(j);
 		for (var k = 0; k < content.length; k++) {
 			if (content[k].match(taskIndex) != null) {
@@ -22,47 +22,8 @@ function sortByTask(bigTask) {
 }
 
 function getTitle(index) {
-	switch(index) {
-		case 0:
-			return "任务一";
-			break;
-		case 1:
-			return "任务二";
-			break;
-		case 2:
-			return "任务三";
-			break;
-		case 3:
-			return "任务四";
-			break;
-		case 4:
-			return "任务五";
-			break;
-		case 5:
-			return "任务六";
-			break;
-		case 6:
-			return "任务七";
-			break;
-		case 7:
-			return "任务八";
-			break;
-		case 8:
-			return "任务九";
-			break;
-		case 9:
-			return "任务十";
-			break;
-		case 10:
-			return "任务十一";
-			break;
-		case 11:
-			return "任务十二";
-			break;
-		default:
-			return "ERROR";
-			break;
-	}
+	var tasks = ["任务一：", "任务二：", "任务三：", "任务四：", "任务五：", "任务六：", "任务七：", "任务八：", "任务九：", "任务十：", "任务十一：", "任务十二：", "任务十三：", "任务十四：", "任务十五：", "任务十六：", "任务十七：", "任务十八：", "任务十九：", "任务二十：", "任务二十一：", "任务二十二：", "任务二十三：", "任务二十四：", "任务二十五：", "任务二十六：", "任务二十七：", "任务二十八：", "任务二十九：", "任务三十：", "任务三十一：", "任务三十二：", "任务三十三：", "任务三十四：", "任务三十五：", "任务三十六：", "任务三十七：", "任务三十八：", "任务三十九：", "任务四十：", "任务四十一：", "任务四十二：", "任务四十三：", "任务四十四：", "任务四十五：", "任务四十六：", "任务四十七：", "任务四十八：", "任务四十九："];
+	return tasks[index];
 }
 
 function main() {
