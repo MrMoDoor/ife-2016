2b623b89e6c85039d0b4046ff5bfa07a45c29113
harryfyodor <wuhaiweideyouxiang@163.com>
2016-04-17 21:01:07 +0800

    add harryfyodor

--- personal/harryfyodor/task3/task0003-44/gulpfile.js
+++ personal/harryfyodor/task3/task0003-44/gulpfile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var gulp = require('gulp'),
     gulpImagemin = require('gulp-imagemin');
 
@@ -6,13 +5,4 @@ gulp.task('image', function () {
     gulp.src('img/**')
         .pipe(gulpImagemin())
         .pipe(gulp.dest('images'));
-=======
-var gulp = require('gulp'),
-    gulpImagemin = require('gulp-imagemin');
-
-gulp.task('image', function () {
-    gulp.src('img/**')
-        .pipe(gulpImagemin())
-        .pipe(gulp.dest('images'));
->>>>>>> 467e6f6c220461185c8617ae4e0e8fef15fff009
 });
